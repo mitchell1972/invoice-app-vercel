5defe478340ced34684a0072f16dbd227ff7d615
Mitchell Agoma <13911129+mitchell1972@users.noreply.github.com>
2025-08-09 15:14:32 -0700

    fix: use loginSimple component in main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LoginPage from './login.tsx';
+import LoginPage from './loginSimple.tsx';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
